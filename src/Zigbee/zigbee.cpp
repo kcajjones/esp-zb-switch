6f414238ed92b59b2e6797c8d84c41bb8b852bc5
kcajjones <50169142+kcajjones@users.noreply.github.com>
2025-01-10 10:29:35 +0000

    Update zigbee.cpp
    Make changes for device to be Zigbee Router mode instead of End Device mode.

--- src/Zigbee/zigbee.cpp
+++ src/Zigbee/zigbee.cpp
@@ -1,18 +1,3 @@
-// Copyright 2024 Espressif Systems (Shanghai) PTE LTD
-// Modifications Copyright 2024 Skye Harris
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 #include <Arduino.h>
 #include "Zigbee/zigbee.h"
 
@@ -195,8 +180,8 @@ static esp_zb_ep_list_t *createSensorEndpoint(uint8_t endpoint_id) {
 static void taskZigbeeMain(void *pvParameters) {
     esp_zb_set_trace_level_mask(ESP_ZB_TRACE_LEVEL_CRITICAL, ESP_ZB_TRACE_SUBSYSTEM_MAC | ESP_ZB_TRACE_SUBSYSTEM_APP);
 
-    // Initialise our configuration
-    esp_zb_cfg_t zb_nwk_cfg = ESP_ZB_ZED_CONFIG();
+    // Initialize Zigbee configuration for a router
+    esp_zb_cfg_t zb_nwk_cfg = ESP_ZB_ZCZR_CONFIG();  // Changed from ESP_ZB_ZED_CONFIG to ESP_ZB_ZCZR_CONFIG
     esp_zb_init(&zb_nwk_cfg);
 
     /* Create customized sensor endpoint */
@@ -208,17 +193,72 @@ static void taskZigbeeMain(void *pvParameters) {
     /* Register our action callback */
     esp_zb_core_action_handler_register(onZigbeeAction);
 
-    /* Config the reporting info  */
-    // configureReporting(ESP_ZB_ZCL_CLUSTER_ID_FAN_CONTROL,ESP_ZB_ZCL_CLUSTER_SERVER_ROLE,0x0,ESP_ZB_ZCL_CMD_DIRECTION_TO_CLI);
+    /* Config the reporting info */
+    // configureReporting(ESP_ZB_ZCL_CLUSTER_ID_FAN_CONTROL, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE, 0x0, ESP_ZB_ZCL_CMD_DIRECTION_TO_CLI);
 
     esp_zb_set_primary_network_channel_set(ESP_ZB_PRIMARY_CHANNEL_MASK);
 
     // Erase NVRAM before creating connection to new Coordinator
-    // esp_zb_nvram_erase_at_start(true); //Comment out this line to erase NVRAM data if you are conneting to new Coordinator
+    // esp_zb_nvram_erase_at_start(true);  // Comment out this line to erase NVRAM data if you are connecting to new Coordinator
 
     ESP_ERROR_CHECK(esp_zb_start(false));
 
     esp_zb_stack_main_loop();
+}
+
+void esp_zb_app_signal_handler(esp_zb_app_signal_t *signal_struct) {
+    uint32_t *p_sg_p = signal_struct->p_app_signal;
+    esp_err_t err_status = signal_struct->esp_err_status;
+    esp_zb_app_signal_type_t sig_type = (esp_zb_app_signal_type_t)*p_sg_p;
+
+    switch (sig_type) {
+        case ESP_ZB_ZDO_SIGNAL_SKIP_STARTUP:
+            log_i("Zigbee stack initialized");
+            esp_zb_bdb_start_top_level_commissioning(ESP_ZB_BDB_MODE_INITIALIZATION);
+            break;
+
+        case ESP_ZB_BDB_SIGNAL_DEVICE_FIRST_START:
+        case ESP_ZB_BDB_SIGNAL_DEVICE_REBOOT:
+            if (err_status == ESP_OK) {
+                log_i("Start network steering");
+                log_i("Device started up in %sfactory-reset mode", esp_zb_bdb_is_factory_new() ? "" : "non ");
+
+                // Attach our identify handler
+                esp_zb_identify_notify_handler_register(HA_ESP_SENSOR_ENDPOINT, &onZigbeeIdentify);
+
+                if (esp_zb_bdb_is_factory_new()) {
+                    log_i("Start network steering");
+                    esp_zb_bdb_start_top_level_commissioning(ESP_ZB_BDB_MODE_NETWORK_STEERING);
+                } else {
+                    log_i("Device rebooted");
+                }
+            } else {
+                log_w("Failed to initialize Zigbee stack (status: %s)", esp_err_to_name(err_status));
+
+                // Restart after 5 seconds
+                delay(5000);
+                esp_restart();
+            }
+            break;
+
+        case ESP_ZB_BDB_SIGNAL_STEERING:
+            if (err_status == ESP_OK) {
+                esp_zb_ieee_addr_t extended_pan_id;
+                esp_zb_get_extended_pan_id(extended_pan_id);
+                log_i("Joined network successfully (Extended PAN ID: %02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x, PAN ID: 0x%04hx, Channel: %d, Short Address: 0x%04hx)",
+                      extended_pan_id[7], extended_pan_id[6], extended_pan_id[5], extended_pan_id[4], extended_pan_id[3], extended_pan_id[2], extended_pan_id[1],
+                      extended_pan_id[0], esp_zb_get_pan_id(), esp_zb_get_current_channel(), esp_zb_get_short_address());
+            } else {
+                log_w("Network steering was not successful (status: %s)", esp_err_to_name(err_status));
+                log_i("Retrying network steering...");
+                esp_zb_scheduler_alarm((esp_zb_callback_t)bdb_start_top_level_commissioning_cb, ESP_ZB_BDB_MODE_NETWORK_STEERING, 1000);
+            }
+            break;
+
+        default:
+            log_i("ZDO signal: %s (0x%x), status: %s", esp_zb_zdo_signal_to_string(sig_type), sig_type, esp_err_to_name(err_status));
+            break;
+    }
 }
 
 // External interface functions
